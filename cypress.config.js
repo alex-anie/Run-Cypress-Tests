42e3504ef0c2943081c4662b0c14c98f219b7bba
alex-anie <ocxigin@gmail.com>
2025-05-02 17:25:44 +0100

    Cypress test record was performed with cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'na1dw2',
   reporter: 'cypress-mochawesome-reporter',
   reporterOptions: {
     charts: true,
